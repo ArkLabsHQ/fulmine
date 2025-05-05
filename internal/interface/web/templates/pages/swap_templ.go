163a295e13eb8e9bebe95b6f73699691e32b92d6
João Bordalo <bordalix@users.noreply.github.com>
2025-05-05 11:03:20 +0100

    add hr

--- internal/interface/web/templates/pages/swap_templ.go
+++ internal/interface/web/templates/pages/swap_templ.go
@@ -941,8 +941,16 @@ func SwapHistoryBodyContent(swaps []types.Swap) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		} else {
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 65, "<hr class=\"border border-1 border-white/10 w-full\">")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
 			for _, swap := range swaps {
 				templ_7745c5c3_Err = SwapHistoryLine(swap).Render(ctx, templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 66, " <hr class=\"border border-1 border-white/10 w-full\">")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
