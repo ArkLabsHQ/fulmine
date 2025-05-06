26aba4b05ee11d81bac6d60b5f4fba2706a807a6
João Bordalo <bordalix@users.noreply.github.com>
2025-05-06 13:03:27 +0100

    adds pagination to swap history

--- internal/interface/web/server.go
+++ internal/interface/web/server.go
@@ -94,6 +94,7 @@ func NewService(appSvc *application.Service, stopCh chan struct{}, sentryEnabled
 	svc.GET("/swap", svc.swap)
 	svc.GET("/swap/:active", svc.swapActive)
 	svc.GET("/swapHistory/", svc.swapHistory)
+	svc.GET("/swaps/:lastId", svc.getSwaps)
 	svc.GET("/tx/:txid", svc.getTx)
 	svc.GET("/txs", svc.getTxs)
 	svc.GET("/unlock", svc.unlock)
