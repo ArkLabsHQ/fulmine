56dc56e9215cbcdf24d2204c5947c0dce91ff3ee
Joshua Aruokhai <joshuaaruokhaitech@gmail.com>
2025-06-16 17:24:30 +0100

    remove unnecessary function

--- internal/infrastructure/db/sqlite/subscribed_script_repo.go
+++ internal/infrastructure/db/sqlite/subscribed_script_repo.go
@@ -92,13 +92,3 @@ func (r *subscribedScriptRepository) Close() {
 	// nolint
 	r.db.Close()
 }
-
-// contains checks if a slice contains a given string.
-func contains(slice []string, item string) bool {
-	for _, s := range slice {
-		if s == item {
-			return true
-		}
-	}
-	return false
-}
