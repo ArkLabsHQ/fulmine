43c7249b2f899d815b47e1673c62db553c237b0f
altafan <18440657+altafan@users.noreply.github.com>
2025-07-14 02:48:34 +0200

    Fixes after review

--- internal/infrastructure/cln/utils.go
+++ internal/infrastructure/cln/utils.go
@@ -11,6 +11,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func decodeClnConnectUrl(clnConnectUrl string) (rootCert, privateKey, certChain, host string, err error) {
@@ -25,9 +26,15 @@ func decodeClnConnectUrl(clnConnectUrl string) (rootCert, privateKey, certChain,
 	certChain = toBase64(u.Query().Get("certChain"))   // client.pem
 	privateKey = toBase64(u.Query().Get("privateKey")) // client-key.pem
 
-	rootCert = "-----BEGIN CERTIFICATE-----\n" + rootCert + "\n-----END CERTIFICATE-----"
-	certChain = "-----BEGIN CERTIFICATE-----\n" + certChain + "\n-----END CERTIFICATE-----"
-	privateKey = "-----BEGIN PRIVATE KEY-----\n" + privateKey + "\n-----END PRIVATE KEY-----"
+	if rootCert != "" {
+		rootCert = "-----BEGIN CERTIFICATE-----\n" + rootCert + "\n-----END CERTIFICATE-----"
+	}
+	if certChain != "" {
+		certChain = "-----BEGIN CERTIFICATE-----\n" + certChain + "\n-----END CERTIFICATE-----"
+	}
+	if privateKey != "" {
+		privateKey = "-----BEGIN PRIVATE KEY-----\n" + privateKey + "\n-----END PRIVATE KEY-----"
+	}
 
 	return
 
@@ -39,9 +46,12 @@ func deriveClnConnFromUrl(clnConnectUrl string) (conn *grpc.ClientConn, err erro
 		return nil, fmt.Errorf("error decoding cln connect url: %w", err)
 	}
 
-	creds, err := deriveCreds([]byte(rootCert), []byte(certChain), []byte(privateKey))
-	if err != nil {
-		return nil, fmt.Errorf("error deriving credentials: %w", err)
+	creds := insecure.NewCredentials()
+	if rootCert != "" {
+		creds, err = deriveCreds([]byte(rootCert), []byte(certChain), []byte(privateKey))
+		if err != nil {
+			return nil, fmt.Errorf("error deriving credentials: %w", err)
+		}
 	}
 
 	return grpc.NewClient(host, grpc.WithTransportCredentials(creds))
@@ -53,32 +63,35 @@ func deriveClnConnFromPath(dataDir, host, network string) (conn *grpc.ClientConn
 	rootCertPath := filepath.Join(dataDir, "ca.pem")           // root certificate
 	certChainPath := filepath.Join(dataDir, "client.pem")      // client certificate chain
 	privateKeyPath := filepath.Join(dataDir, "client-key.pem") // client private key
+	creds := insecure.NewCredentials()
+	tlsQueryString := ""
+	if _, err := os.Stat(rootCertPath); err == nil {
+		rootCertBytes, err := os.ReadFile(rootCertPath)
+		if err != nil {
+			return nil, "", err
+		}
+		certChainBytes, err := os.ReadFile(certChainPath)
+		if err != nil {
+			return nil, "", err
+		}
+		privateKeyBytes, err := os.ReadFile(privateKeyPath)
+		if err != nil {
+			return nil, "", err
+		}
 
-	rootCertBytes, err := os.ReadFile(rootCertPath)
-	if err != nil {
-		return nil, "", err
-	}
-	certChainBytes, err := os.ReadFile(certChainPath)
-	if err != nil {
-		return nil, "", err
-	}
-	privateKeyBytes, err := os.ReadFile(privateKeyPath)
-	if err != nil {
-		return nil, "", err
-	}
-
-	creds, err := deriveCreds(rootCertBytes, certChainBytes, privateKeyBytes)
-	if err != nil {
-		return nil, "", fmt.Errorf("error deriving credentials: %w", err)
+		creds, err = deriveCreds(rootCertBytes, certChainBytes, privateKeyBytes)
+		if err != nil {
+			return nil, "", fmt.Errorf("error deriving credentials: %w", err)
+		}
+		tlsQueryString = fmt.Sprintf(
+			"?rootCert=%s&privateKey=%s&certChain=%s",
+			url.QueryEscape(toBase64(string(rootCertBytes))),
+			url.QueryEscape(toBase64(string(privateKeyBytes))),
+			url.QueryEscape(toBase64(string(certChainBytes))))
 	}
 
 	//derive LnConnect URL
-	lnConnectUrl = fmt.Sprintf("clnconnect://%s?rootCert=%s&privateKey=%s&certChain=%s",
-		host,
-		url.QueryEscape(toBase64(string(rootCertBytes))),
-		url.QueryEscape(toBase64(string(privateKeyBytes))),
-		url.QueryEscape(toBase64(string(certChainBytes))),
-	)
+	lnConnectUrl = fmt.Sprintf("clnconnect://%s%s", host, tlsQueryString)
 	conn, err = grpc.NewClient(host, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		return nil, "", fmt.Errorf("error creating grpc client: %w", err)
@@ -88,7 +101,7 @@ func deriveClnConnFromPath(dataDir, host, network string) (conn *grpc.ClientConn
 
 }
 
-func deriveCreds(rootCert []byte, certChain []byte, privateKey []byte) (cred credentials.TransportCredentials, err error) {
+func deriveCreds(rootCert []byte, certChain []byte, privateKey []byte) (credentials.TransportCredentials, error) {
 	caPool := x509.NewCertPool()
 	if !caPool.AppendCertsFromPEM(rootCert) {
 		return nil, fmt.Errorf("could not parse root certificate")
