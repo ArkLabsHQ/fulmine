982d3db599fa5ab2260b6537f58117824d5ed18c
louisinger <louis@vulpem.com>
2025-08-05 14:00:05 +0200

    rework subscription handler

---
+++ internal/core/application/subscription.go
@@ -0,0 +1,241 @@
+package application
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/arkade-os/go-sdk/indexer"
+	indexergrpc "github.com/arkade-os/go-sdk/indexer/grpc"
+	log "github.com/sirupsen/logrus"
+)
+
+type scriptsStore interface {
+	Get(ctx context.Context) ([]string, error)
+	Add(ctx context.Context, subscribedScripts []string) (count int, err error)
+	Delete(ctx context.Context, subscribedScripts []string) (count int, err error)
+}
+
+type subscriptionHandler struct {
+	indexerBaseURL string
+	scripts        scriptsStore
+	onEvent        func(event *indexer.ScriptEvent)
+
+	lock    sync.RWMutex
+	closeFn func()
+	id      string
+}
+
+func newSubscriptionHandler(indexerBaseURL string, scripts scriptsStore, onEvent func(event *indexer.ScriptEvent)) *subscriptionHandler {
+	return &subscriptionHandler{
+		indexerBaseURL: indexerBaseURL,
+		scripts:        scripts,
+		onEvent:        onEvent,
+		lock:           sync.RWMutex{},
+		closeFn:        nil,
+		id:             "",
+	}
+}
+
+func (h *subscriptionHandler) createIndexerClient() (indexer.Indexer, error) {
+	return indexergrpc.NewClient(h.indexerBaseURL)
+}
+
+func (h *subscriptionHandler) subscribe(ctx context.Context, scripts []string) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	count, err := h.scripts.Add(ctx, scripts)
+	if err != nil {
+		return fmt.Errorf("failed to add scripts: %w", err)
+	}
+
+	if count == 0 {
+		return nil
+	}
+
+	log.Debugf("added %d scripts to subscription", count)
+
+	if len(h.id) == 0 {
+		if err := h.start(); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	indexerClient, err := h.createIndexerClient()
+	if err != nil {
+		return fmt.Errorf("failed to create indexer client: %w", err)
+	}
+
+	_, err = indexerClient.SubscribeForScripts(ctx, h.id, scripts)
+	if err != nil {
+		log.WithError(err).Warn("failed to update subscription, retrying...")
+		h.stop()
+		if err := h.start(); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func (h *subscriptionHandler) unsubscribe(ctx context.Context, scripts []string) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	count, err := h.scripts.Delete(ctx, scripts)
+	if err != nil {
+		return fmt.Errorf("failed to remove scripts: %w", err)
+	}
+
+	if count == 0 {
+		return nil
+	}
+
+	log.Debugf("removed %d scripts from subscription", count)
+
+	if len(h.id) == 0 {
+		if err := h.start(); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	indexerClient, err := h.createIndexerClient()
+	if err != nil {
+		return fmt.Errorf("failed to create indexer client: %w", err)
+	}
+
+	err = indexerClient.UnsubscribeForScripts(ctx, h.id, scripts)
+	if err != nil {
+		log.WithError(err).Warn("failed to unsubscribe, retrying...")
+		h.stop()
+		if err := h.start(); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	scripts, err = h.scripts.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get scripts: %w", err)
+	}
+
+	if len(scripts) == 0 {
+		h.stop()
+		return nil
+	}
+
+	return nil
+}
+
+func (h *subscriptionHandler) stop() {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	if h.closeFn != nil {
+		h.closeFn()
+		h.closeFn = nil
+	}
+
+	if len(h.id) != 0 {
+		log.Debugf("stopped subscription %s", h.id)
+		h.id = ""
+	}
+}
+
+func (h *subscriptionHandler) start() error {
+	ctx := context.Background()
+
+	onError := func(err error) {
+		h.stop()
+		log.WithError(err).Warn("retrying in 2 seconds")
+		time.Sleep(2 * time.Second)
+	}
+
+	scripts, err := h.scripts.Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(scripts) == 0 {
+		h.stop()
+		log.Debugf("no scripts to subscribe to, skip starting subscription")
+		return nil
+	}
+
+	go func() {
+		for {
+			if err := h.create(ctx); err != nil {
+				onError(err)
+				continue
+			}
+
+			log.Debugf("created subscription %s", h.id)
+
+			indexerClient, err := h.createIndexerClient()
+			if err != nil {
+				onError(err)
+				continue
+			}
+
+			subscriptionChannel, closeFn, err := indexerClient.GetSubscription(ctx, h.id)
+			if err != nil {
+				onError(err)
+				continue
+			}
+
+			h.closeFn = closeFn
+
+			stopped := false
+			for !stopped {
+
+				select {
+				case <-ctx.Done():
+					return
+				case event := <-subscriptionChannel:
+					if event.Err != nil {
+						onError(event.Err)
+						stopped = true
+						continue
+					}
+
+					log.Debugf("received transaction event: %s for subscription %s", event.Txid, h.id)
+					go h.onEvent(event)
+				}
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (h *subscriptionHandler) create(ctx context.Context) error {
+	var err error
+
+	scripts, err := h.scripts.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get scripts: %w", err)
+	}
+
+	if len(scripts) == 0 {
+		return fmt.Errorf("no scripts to subscribe to")
+	}
+
+	indexerClient, err := h.createIndexerClient()
+	if err != nil {
+		return fmt.Errorf("failed to create indexer client: %w", err)
+	}
+
+	subscriptionId, err := indexerClient.SubscribeForScripts(ctx, "", scripts)
+	if err != nil {
+		return fmt.Errorf("failed to subscribe for scripts: %w", err)
+	}
+
+	h.id = subscriptionId
+
+	return nil
+}
