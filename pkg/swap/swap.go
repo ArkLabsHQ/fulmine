408e059158a0a2172809772fb5293c8673b47ee4
Joshua Aruokhai <joshuaaruokhaitech@gmail.com>
2025-09-24 07:07:29 +0100

    Increase context timeout for submarine swap to 5 minutes

--- pkg/swap/swap.go
+++ pkg/swap/swap.go
@@ -193,7 +193,7 @@ func (h *SwapHandler) submarineSwap(ctx context.Context, invoice string, unilate
 	}
 
 	ws := h.boltzSvc.NewWebsocket()
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	err = ws.ConnectAndSubscribe(ctx, []string{swap.Id}, 5*time.Second)
