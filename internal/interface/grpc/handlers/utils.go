26f011fad9e6ce12ea31e5004ef92986dd086ab7
altafan <18440657+altafan@users.noreply.github.com>
2026-05-11 18:54:22 +0200

    Lint

--- internal/interface/grpc/handlers/utils.go
+++ internal/interface/grpc/handlers/utils.go
@@ -241,8 +241,8 @@ func toNotificationProto(n application.Notification) *pb.Notification {
 		Addresses:  n.Addrs,
 		NewVtxos:   toVtxosProto(n.NewVtxos),
 		SpentVtxos: toVtxosProto(n.SpentVtxos),
-		Txid:       n.TxData.Txid,
-		Tx:         n.TxData.Tx,
+		Txid:       n.Txid,
+		Tx:         n.Tx,
 	}
 	if len(n.Checkpoints) > 0 {
 		notification.Checkpoints = make(map[string]*pb.TxData, len(n.Checkpoints))
