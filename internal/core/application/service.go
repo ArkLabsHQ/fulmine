9b55c02e085625e5bf13501df407ad272ed83cf8
louisinger <louis@vulpem.com>
2026-05-20 09:05:40 +0200

    revert Unlock change

--- internal/core/application/service.go
+++ internal/core/application/service.go
@@ -417,10 +417,6 @@ func (s *Service) UnlockNode(ctx context.Context, password string) error {
 		return nil
 	}
 
-	if err := s.Unlock(ctx, password); err != nil {
-		return err
-	}
-
 	s.syncCh = make(chan types.SyncEvent, 1)
 
 	wg := &sync.WaitGroup{}
@@ -431,6 +427,10 @@ func (s *Service) UnlockNode(ctx context.Context, password string) error {
 		s.syncEvent = &ev
 		s.syncCh <- ev
 	})
+
+	if err := s.Unlock(ctx, password); err != nil {
+		return err
+	}
 
 	s.schedulerSvc.Start()
 	log.Info("scheduler started")
